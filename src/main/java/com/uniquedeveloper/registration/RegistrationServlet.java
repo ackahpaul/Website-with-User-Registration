6510730498f3f00eb3216ce059906a512b62e080
Paul Ackah <ackahpaul@live.com>
2022-11-14 10:04:56 -0600

    update file
    update

---
+++ src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
@@ -0,0 +1,90 @@
+package com.uniquedeveloper.registration;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		// PrintWriter out = response.getWriter();
+		// out.print("Working");
+
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
+
+		RequestDispatcher dispatcher = null;
+		Connection con = null;
+		// PrintWriter out = response.getWriter();
+		// out.print("unmame");
+		// out.print("uemail");
+		// out.print("upwd");
+		// out.print("umobile");
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "pearwindow1980E!");
+			PreparedStatement pst = con
+					.prepareStatement("insert into users(uname,upwd,uemail,umobile) values (?,?,?,?)");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+
+			if (rowCount > 0) {
+
+				request.setAttribute("status", "success");
+
+			} else {
+
+				request.setAttribute("status", "failed");
+
+			}
+
+			dispatcher.forward(request, response);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+
+			if (con == null) {
+
+				return;
+			}
+
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
